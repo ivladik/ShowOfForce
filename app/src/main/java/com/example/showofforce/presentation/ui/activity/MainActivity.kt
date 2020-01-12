3f466725a4a984ac871b62752712a2424bd46838
vfalzan <indivladi120112@gmail.com>
2020-01-12 11:42:37 +0300

    - added navigation component to feature tours

--- app/src/main/java/com/example/showofforce/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/showofforce/presentation/ui/activity/MainActivity.kt
@@ -2,24 +2,45 @@ package com.example.showofforce.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.feature_tours.di.ToursSubcomponent
 import com.example.showofforce.R
 import com.example.showofforce.di.FeatureInjector
+import kotlinx.android.synthetic.main.ac_main.*
 
 // TODO: tests, jacoco
 // TODO: add icons
-// TODO: rotate screen fix
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
-        navigateToToursFragment()
+        setupNavigation()
+//        navigateToToursFragment()
+        FeatureInjector.createToursSubcomponent()
     }
 
-    // TODO: Single Activity, Navigation Component
+    private fun setupNavigation() {
+        val navigationController = findNavController(R.id.navigationController)
+        setupActionBarWithNavController(navigationController)
+        navigationView.setupWithNavController(navigationController)
+    }
+
+    override fun onDestroy() {
+        if (isFinishing) {
+            ToursSubcomponent.release()
+        }
+        super.onDestroy()
+    }
+
+    // TODO: удалить starter, activity
     private fun navigateToToursFragment() {
         FeatureInjector.createToursSubcomponent()
             ?.starter()
             ?.start(this)
     }
+
+    override fun onSupportNavigateUp(): Boolean = findNavController(R.id.navigationController).navigateUp()
 }
